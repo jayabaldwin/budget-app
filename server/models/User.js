c76080f72397e150069eebb4dfd4da719116f7d5
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-04-30 20:02:03 +1000

    FInalised models and schemna

--- server/models/User.js
+++ server/models/User.js
@@ -1,61 +1,61 @@
-const {Schema, model } = require('mongoose');
-const Expense = require('./Expense');
+const { Schema, model } = require("mongoose");
+const MoneyOut = require("./MoneyOut");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  initials: {
+    type: String,
+    get: function () {
+      return this.firstName[0].toUpperCase() + this.lastName[0].toUpperCase();
     },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must be an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  finances: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Finance",
     },
-    initials: {
-        type: String,
-        get: function(){
-            return this.firstName[0].toUpperCase()+this.lastName[0].toUpperCase();
-        },
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Must be an email address!'],
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-    },
-    expense: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Expense',
-    }],
+  ],
 });
 
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
-
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
-userSchema.methods.isCorrectPassword = async function (password){
-    return bcrypt.compare(password, this.password);
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
 };
 
-
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
