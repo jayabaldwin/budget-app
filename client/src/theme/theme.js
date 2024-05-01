21dee5bf0c3690db8a362d28c82273e4cb41a071
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-05-02 09:51:16 +1000

    Theme outline

---
+++ client/src/theme/theme.js
@@ -0,0 +1,19 @@
+// import { createTheme } from "@mui/material/styles";
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: "#2196f3",
+//     },
+//     secondary: {
+//       main: "#ff4081",
+//     },
+//     // Other palette colors...
+//   },
+//   typography: {
+//     // Define typography settings...
+//   },
+//   // Other theme properties...
+// });
+
+// export default theme;
