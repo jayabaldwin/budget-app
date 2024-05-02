4251c603d7bbc7b2d583d0a12669e1d0e7011d11
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-05-02 10:18:19 +1000

    Added sidenav and appbar template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 // import { ThemeProvider } from '@mui/material/styles';
 // import theme from './theme/theme.js'
 import './App.css'
+import LayoutNav from './components/layout/LayoutNav.jsx'
 import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     {/* <ThemeProvider theme={theme}> */}
       <CssBaseline />
       <main>
+        <LayoutNav />
         <Outlet />
       </main>
     {/* </ThemeProvider> */}
