b3fdd463183dc5cdc0451df8e24849d8cd10433d
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-05-02 18:26:40 +1000

    Configured routes to connect  with Apollo

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,153 +1,152 @@
-const { gql } = require("apollo-server-express");
-const { gql } = require("apollo-server-express");
+// const { gql } = require("apollo-server-express");
+// const { gql } = require("apollo-server-express");
 
-const typeDefs = gql`
-  type User {
-    _id: ID
-    firstname: String!
-    lastname: String!
-    initials: String
-    email: String!
-    password: String
-    finances: [Finance]
-  }
+// const typeDefs = gql`
+//   type User {
+//     _id: ID
+//     firstname: String!
+//     lastname: String!
+//     initials: String
+//     email: String!
+//     password: String
+//     finances: [Finance]
+//   }
 
-  type Finance {
-    _id: ID
-    balance: Int!
-    income: [Income]
-    savings: [Savings]
-    moneyOut: [MoneyOut]
-  }
+//   type Finance {
+//     _id: ID
+//     balance: Int!
+//     income: [Income]
+//     savings: [Savings]
+//     moneyOut: [MoneyOut]
+//   }
 
-  type Income {
-    _id: ID
-    amount: Int!
-    description: String!
-    date: Date
-  }
+//   type Income {
+//     _id: ID
+//     amount: Int!
+//     description: String!
+//     date: Date
+//   }
 
-  type Savings {
-    _id: ID
-    amount: Int!
-    description: String!
-    date: Date
-  }
-  type User {
-    _id: ID
-    firstname: String!
-    lastname: String!
-    initials: String
-    email: String!
-    password: String
-    finances: [Finance]
-  }
+//   type Savings {
+//     _id: ID
+//     amount: Int!
+//     description: String!
+//     date: Date
+//   }
+//   type User {
+//     _id: ID
+//     firstname: String!
+//     lastname: String!
+//     initials: String
+//     email: String!
+//     password: String
+//     finances: [Finance]
+//   }
 
-  type Finance {
-    _id: ID
-    balance: Int!
-    income: [Income]
-    savings: [Savings]
-    moneyOut: [MoneyOut]
-  }
+//   type Finance {
+//     _id: ID
+//     balance: Int!
+//     income: [Income]
+//     savings: [Savings]
+//     moneyOut: [MoneyOut]
+//   }
 
-  type Income {
-    _id: ID
-    amount: Int!
-    description: String!
-    date: Date
-  }
+//   type Income {
+//     _id: ID
+//     amount: Int!
+//     description: String!
+//     date: Date
+//   }
 
-  type Savings {
-    _id: ID
-    amount: Int!
-    description: String!
-    date: Date
-  }
+//   type Savings {
+//     _id: ID
+//     amount: Int!
+//     description: String!
+//     date: Date
+//   }
 
-  type MoneyOut {
-    _id: ID
-    amount: Int!
-    description: String!
-    date: Date
-    category: [String]!
-  }
+//   type MoneyOut {
+//     _id: ID
+//     amount: Int!
+//     description: String!
+//     date: Date
+//     category: [String]!
+//   }
 
-  # graphQL basically knows these belong in the category: [] array
-  enum CategoryEnum {
-    Home
-    Utilities
-    Transport
-    Groceries
-    EatingOut
-    Shopping
-    Entertainment
-    Health
-    Education
-    Travel
-    Business
-    Miscellaneous
-  }
-  type MoneyOut {
-    _id: ID
-    amount: Int!
-    description: String!
-    date: Date
-    category: [String]!
-  }
+//   # graphQL basically knows these belong in the category: [] array
+//   enum CategoryEnum {
+//     Home
+//     Utilities
+//     Transport
+//     Groceries
+//     EatingOut
+//     Shopping
+//     Entertainment
+//     Health
+//     Education
+//     Travel
+//     Business
+//     Miscellaneous
+//   }
+//   type MoneyOut {
+//     _id: ID
+//     amount: Int!
+//     description: String!
+//     date: Date
+//     category: [String]!
+//   }
 
-  # graphQL basically knows these belong in the category: [] array
-  enum CategoryEnum {
-    Home
-    Utilities
-    Transport
-    Groceries
-    EatingOut
-    Shopping
-    Entertainment
-    Health
-    Education
-    Travel
-    Business
-    Miscellaneous
-  }
+//   # graphQL basically knows these belong in the category: [] array
+//   enum CategoryEnum {
+//     Home
+//     Utilities
+//     Transport
+//     Groceries
+//     EatingOut
+//     Shopping
+//     Entertainment
+//     Health
+//     Education
+//     Travel
+//     Business
+//     Miscellaneous
+//   }
 
-  type Auth {
-    token: String
-    user: User
-  }
-  type Auth {
-    token: String
-    user: User
-  }
+//   type Auth {
+//     token: String
+//     user: User
+//   }
+//   type Auth {
+//     token: String
+//     user: User
+//   }
 
-  type Query {
-    users: [User]
-    user(email: String!): User
-  }
-  type Query {
-    users: [User]
-    user(email: String!): User
-  }
+//   type Query {
+//     users: [User]
+//     user(email: String!): User
+//   }
+//   type Query {
+//     users: [User]
+//     user(email: String!): User
+//   }
 
-  type Mutation {
+//   type Mutation {
 
-    addUser(
-      firstname: String!
-      lastname: String!
-      email: String!
-      password: String!
-    ): Auth
+//     addUser(
+//       firstname: String!
+//       lastname: String!
+//       email: String!
+//       password: String!
+//     ): Auth
 
-    addBalance (
-      email: String!
-      balance: Int!
-    ): Finance
-}
+//     addBalance (
+//       email: String!
+//       balance: Int!
+//     ): Finance
+// }
 
+//   scalar Date
+//   scalar Date
+// `;
 
-  scalar Date
-  scalar Date
-`;
-
-module.exports = typeDefs;
+// module.exports = typeDefs;
