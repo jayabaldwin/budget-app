6e2d90049bfd1f956de18b4959f18ddcda005cf7
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-05-09 13:11:15 +1000

    Added notification for sign up and error handling

--- client/src/components/login/SignUp.jsx
+++ client/src/components/login/SignUp.jsx
@@ -4,33 +4,64 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import Snackbar from '@mui/material/Snackbar';
+import SnackbarContent from '@mui/material/SnackbarContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import Auth from '../../utils/auth';
 import { ADD_USER } from '../../utils/mutations';
 
-const styles = {
-  contactFrame: {
-    backgroundColor: '#ffffff6b',
-    borderRadius: '15px',
-    padding: '30px',
-    marginTop: '20px',
-    width: '100%',
-    maxWidth: '600px'
-  }
-};
+function Notification({ message, handleClose }) {
+  const action = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      onClick={handleClose}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+
+  return (
+    <Snackbar
+      open={true}
+      autoHideDuration={3000}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+    >
+      <SnackbarContent
+        message={message}
+        action={action}
+        style={{ backgroundColor: '#673ab7', color: '#ffffff' }} // Change the background color here
+      />
+    </Snackbar>
+  );
+}
 
 function Signup(props) {
-  
   const [formState, setFormState] = useState({
-    email: '', 
+    firstname: '',
+    lastname: '',
+    email: '',
     password: ''
   });
-  const [addUser] = useMutation(ADD_USER);
+
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    try {
       const mutationResponse = await addUser({
         variables: {
           firstname: formState.firstname,
@@ -39,75 +70,80 @@ function Signup(props) {
           password: formState.password,
         },
       });
-    console.log(mutationResponse);
-    const token = mutationResponse.data.addUser.token;
-    Auth.login(token);
+      const token = mutationResponse.data.addUser.token;
+      Auth.login(token);
+    } catch (error) {
+      setErrorMessage('Invalid email or password');
+      setOpenSnackbar(true);
+    }
   };
 
   const handleChange = (event) => {
-    const {name, value } = event.target;
+    const { name, value } = event.target;
     setFormState({
       ...formState,
       [name]: value,
     });
   };
-  
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
 
   return (
     <Box
-      sx={{marginTop: '10rem'}}
+      sx={{ marginTop: '10rem' }}
       noValidate
       autoComplete="off"
     >
-        <form 
-          onSubmit={handleFormSubmit}
-          className='loginForm'
-          // style={styles.contactFrame}
-          >
-          <Stack spacing={2}>
+      <form
+        onSubmit={handleFormSubmit}
+        className='loginForm'
+      >
+        <Stack spacing={2}>
           <Typography variant="h4" component="div" sx={{ flexGrow: 1, textAlign: 'flex-start' }}>
-             Account Creation.
+            Account Creation.
           </Typography>
-            <TextField
-              required
-              id="outlined-required"
-              label="First Name"
-              name='firstname'
-              onChange={handleChange}
-
-            />
-            <TextField
-              required
-              id="outlined-required"
-              label="Last Name"
-              name='lastname'
-              onChange={handleChange}
-
-            />
-            <TextField
-              required
-              id="outlined-required"
-              label="Email"
-              name='email'
-              onChange={handleChange}
-
-            />
-            <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              autoComplete="Password"
-              name='password'
-              onChange={handleChange}
-            />
-            <Button 
-              variant="contained"
-              type="submit" 
-              endIcon={<SendIcon />}>
-                Make My Account!
-            </Button>
-          </Stack>
-        </form>
+          <TextField
+            required
+            id="outlined-required"
+            label="First Name"
+            name='firstname'
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Last Name"
+            name='lastname'
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            name='email'
+            onChange={handleChange}
+          />
+          <TextField
+          required
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="Password"
+            name='password'
+            onChange={handleChange}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            endIcon={<SendIcon />}
+          >
+            Make My Account!
+          </Button>
+        </Stack>
+      </form>
+      {openSnackbar && <Notification message={errorMessage} handleClose={handleCloseSnackbar} />}
     </Box>
   );
 }
