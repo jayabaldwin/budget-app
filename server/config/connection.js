be75bf9a543fd918d4fe8e209d0733563bab2fc9
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-05-09 18:52:44 +1000

    Added walkthrough to Readme

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget-app"
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://0.0.0.0/0/budget-app");
 
 module.exports = mongoose.connection;
